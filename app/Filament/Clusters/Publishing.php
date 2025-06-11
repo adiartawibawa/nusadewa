5334631640204c4c3fbfdfcd314625ed1a5ab7b8
Adi Arta Wibawa <75978314+adiartawibawa@users.noreply.github.com>
2025-06-11 11:39:04 +0800

    Add navigation group to Publishing and Settings clusters

--- app/Filament/Clusters/Publishing.php
+++ app/Filament/Clusters/Publishing.php
@@ -7,4 +7,6 @@ use Filament\Clusters\Cluster;
 class Publishing extends Cluster
 {
     protected static ?string $navigationIcon = 'heroicon-o-newspaper';
+
+    protected static ?string $navigationGroup = 'Content Management';
 }
