eedc75079f44472e971a939ab7e5931e6611e761
adiartawibawa <surat.buat.adi@gmail.com>
2025-07-10 09:51:05 +0800

    refactor: restructure error messages for improved organization and clarity in Indonesian localization

--- lang/id/errors.php
+++ lang/id/errors.php
@@ -1,9 +1,32 @@
 <?php
 
 return [
-    'title' => 'Halaman Tidak Ditemukan',
-    'code' => '404 error',
-    'description' => 'Maaf, halaman yang Anda cari tidak dapat ditemukan. Berikut beberapa tautan yang mungkin membantu:',
-    'back' => 'Kembali',
-    'home' => 'Ke Beranda',
+    '404' => [
+        'title' => 'Halaman Tidak Ditemukan',
+        'code' => 'Error 404',
+        'description' => 'Maaf, halaman yang Anda cari tidak dapat ditemukan. Berikut beberapa tautan yang mungkin membantu:',
+        'back' => 'Kembali',
+        'home' => 'Ke Beranda',
+    ],
+    '500' => [
+        'title' => 'Kesalahan Server Internal',
+        'code' => 'Error 500',
+        'description' => 'Terjadi kesalahan tak terduga pada server. Silakan coba lagi nanti.',
+        'back' => 'Kembali',
+        'home' => 'Ke Beranda',
+    ],
+    '403' => [
+        'title' => 'Terlarang',
+        'code' => 'Error 403',
+        'description' => 'Anda tidak memiliki izin untuk mengakses halaman ini. Jika Anda merasa ini adalah kesalahan, harap hubungi administrator situs.',
+        'back' => 'Kembali',
+        'home' => 'Ke Beranda',
+    ],
+    '419' => [
+        'title' => 'Halaman Kedaluwarsa',
+        'code' => 'Error 419',
+        'description' => 'Sesi Anda telah berakhir. Silakan segarkan halaman dan coba lagi.',
+        'back' => 'Kembali',
+        'home' => 'Ke Beranda',
+    ]
 ];
